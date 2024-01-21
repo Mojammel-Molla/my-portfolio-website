a05b57ff045ccc99eb9ed3d54d1b035f0305e49b
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-21 23:45:08 +0600

    contact Info Alignment adjusted

--- src/shared/ContactInfo.jsx
+++ src/shared/ContactInfo.jsx
@@ -1,19 +1,19 @@
-// import { FaEnvelope, FaLocationPin, FaPhone } from 'react-icons/fa6';
+import { FaEnvelope, FaLocationPin, FaPhone } from 'react-icons/fa6';
 const ContactInfo = () => {
   return (
-    <div className="text-center font-medium my-5">
-      <h3 className="">
-        {/* <FaPhone /> */}
-        Phone:(+880)1967675977
-      </h3>
-      <h4>
-        {/* <FaEnvelope /> */}
-        Email:Mojammelmolla897@gmail.com
-      </h4>
-      <p>
-        {/* <FaLocationPin /> */}
-        Munshiganj, Dhaka, Bangladesh
-      </p>
+    <div className="mx-auto font-medium my-5">
+      <div className="flex gap-2 justify-center">
+        <FaPhone />
+        <h3 className="">Phone:(+880)1967675977</h3>
+      </div>
+      <div className="flex gap-2 justify-center">
+        <FaEnvelope />
+        <h4>Email:Mojammelmolla897@gmail.com</h4>
+      </div>
+      <div className="flex gap-2 justify-center">
+        <FaLocationPin />
+        <h4>Munshiganj, Dhaka, Bangladesh</h4>
+      </div>
     </div>
   );
 };
